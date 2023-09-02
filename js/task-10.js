3faa6b953f1e4dcd27a74d4270fc8736a85c39fe
whoisanetta <122131174+whoisanetta@users.noreply.github.com>
2023-09-02 12:19:10 +0200

    test check 2

--- js/task-10.js
+++ js/task-10.js
@@ -3,24 +3,3 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-const button = document.querySelector("button");
-const bodySite = document.querySelector("body");
-const nameColor = document.querySelector(".color");
-
-button.addEventListener("click", onClickButton);
-
-function onClickButton() {
-  bodySite.style.backgroundColor = getRandomHexColor();
-}
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-function backgroundColorBtnHandler(event) {
-  document.body.style.backgroundColor = textContainer.textContent =
-    getRandomHexColor();
-}
