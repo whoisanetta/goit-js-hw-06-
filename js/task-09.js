0e954229e82a126eb02538fae3bfe467686ada40
whoisanetta <122131174+whoisanetta@users.noreply.github.com>
2023-09-02 12:09:21 +0200

    task 9 changes

--- js/task-09.js
+++ js/task-09.js
@@ -8,9 +8,10 @@ function onClickButton() {
   bodySite.style.backgroundColor = getRandomHexColor();
 }
 
-function backgroundColorBtnHandler(event) {
-  document.body.style.backgroundColor = textContainer.textContent =
-    getRandomHexColor();
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 function backgroundColorBtnHandler(event) {
